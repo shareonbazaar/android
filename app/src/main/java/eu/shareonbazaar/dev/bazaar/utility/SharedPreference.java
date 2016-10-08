96f219f769c670c6ed0cc7142b9b3272d50b0c48
Damilola Odujoko <odujoko.dml30@gmail.com>
2016-10-08 10:29:59 +0100

    Added SharedPreference to save token

---
+++ app/src/main/java/eu/shareonbazaar/dev/bazaar/utility/SharedPreference.java
@@ -0,0 +1,28 @@
+package eu.shareonbazaar.dev.bazaar.utility;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Larry on 10/8/2016.
+ */
+public class SharedPreference {
+    SharedPreferences sharedPref;
+    private static final String PREF_NAME = "PREF";
+
+    public SharedPreference(Context context){
+        sharedPref = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+    }
+
+    public void saveToken(String key, String value){
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    public String retrieveToken(String key){
+        String result = sharedPref.getString(key, "");
+
+        return result;
+    }
+}
