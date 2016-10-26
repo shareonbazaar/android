9afbcbfa563271cf4e9a8b7f5c5d48fc0439409c
Damilola Odujoko <odujoko.dml30@gmail.com>
2016-10-26 23:27:09 +0100

    Updated style and colors

---
+++ app/src/main/java/eu/shareonbazaar/dev/bazaar/activity/IndividualProfile.java
@@ -0,0 +1,14 @@
+package eu.shareonbazaar.dev.bazaar.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import eu.shareonbazaar.dev.bazaar.R;
+
+public class IndividualProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_profile);
+    }
+}
