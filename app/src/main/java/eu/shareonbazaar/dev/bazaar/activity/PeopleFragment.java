995a81e8f2da6d48f1014336f573ee1405bb3410
Damilola Odujoko <odujoko.dml30@gmail.com>
2016-10-26 22:51:47 +0100

    Designed people's fragment and changed colors

--- app/src/main/java/eu/shareonbazaar/dev/bazaar/activity/PeopleFragment.java
+++ app/src/main/java/eu/shareonbazaar/dev/bazaar/activity/PeopleFragment.java
@@ -3,107 +3,103 @@ package eu.shareonbazaar.dev.bazaar.activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import java.util.HashMap;
+import java.util.List;
 
 import eu.shareonbazaar.dev.bazaar.R;
+import eu.shareonbazaar.dev.bazaar.model.User;
+import eu.shareonbazaar.dev.bazaar.network.RetrofitTemplate;
+import eu.shareonbazaar.dev.bazaar.network.UserService;
+import eu.shareonbazaar.dev.bazaar.ui.RecyclerAdapter;
+import eu.shareonbazaar.dev.bazaar.utility.SharedPreference;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PeopleFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PeopleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class PeopleFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class PeopleFragment extends Fragment implements RecyclerAdapter.ClickListener{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private FrameLayout frameLayout;
+    private RecyclerView recyclerView;
 
     public PeopleFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PeopleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PeopleFragment newInstance(String param1, String param2) {
-        PeopleFragment fragment = new PeopleFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    private void loadUsers(List<User> users){
+        RecyclerAdapter adapter = new RecyclerAdapter(getActivity(), users);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    @Override
+    public void itemClicked(View view, int position) {
+        Snackbar snackbar = Snackbar
+                .make(frameLayout, "Click working", Snackbar.LENGTH_LONG);
+
+        snackbar.show();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_people, container, false);
-    }
+        frameLayout = (FrameLayout)inflater.inflate(R.layout.fragment_people, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        SharedPreference sharedPreference = new SharedPreference(getContext());
+        String token = sharedPreference.retrieveToken("TOKEN");
+
+        recyclerView = (RecyclerView)frameLayout.findViewById(R.id.user_list);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        UserService service = RetrofitTemplate.retrofit.create(UserService.class);
+        service.getUsers(token, new HashMap<String, String>())
+                .enqueue(new Callback<List<User>>() {
+                    @Override
+                    public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                        //Log.d("LOG_TAG", response.body().get(0).getName());
+                        loadUsers(response.body());
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<User>> call, Throwable t) {
+                        Snackbar snackbar = Snackbar
+                                .make(frameLayout, "Data retrieval failed!", Snackbar.LENGTH_LONG);
+
+                        snackbar.show();
+                        String message = t.toString();
+                        Log.d("LOG_TAG", message);
+                    }
+                });
+
+        return frameLayout;
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
--- app/src/main/java/eu/shareonbazaar/dev/bazaar/activity/UsersActivity.java
+++ app/src/main/java/eu/shareonbazaar/dev/bazaar/activity/UsersActivity.java
@@ -1,84 +1,41 @@
 package eu.shareonbazaar.dev.bazaar.activity;
 
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.View;
-import android.widget.LinearLayout;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import eu.shareonbazaar.dev.bazaar.R;
-import eu.shareonbazaar.dev.bazaar.model.User;
-import eu.shareonbazaar.dev.bazaar.network.RetrofitTemplate;
-import eu.shareonbazaar.dev.bazaar.network.UserService;
-import eu.shareonbazaar.dev.bazaar.ui.RecyclerAdapter;
-import eu.shareonbazaar.dev.bazaar.utility.SharedPreference;
-import okhttp3.Interceptor;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import eu.shareonbazaar.dev.bazaar.utility.ViewPagerAdapter;
 
-public class UsersActivity extends AppCompatActivity implements RecyclerAdapter.ClickListener  {
-
-    private LinearLayout linearLayout;
-    private RecyclerView recyclerView;
+public class UsersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        SharedPreference sharedPreference = new SharedPreference(getApplication());
-        String token = sharedPreference.retrieveToken("TOKEN");
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager_main);
+        initializeViewPager(viewPager, toolbar);
 
-        linearLayout = (LinearLayout)findViewById(R.id.user_layout);
-        recyclerView = (RecyclerView) findViewById(R.id.user_list);
-        recyclerView.setHasFixedSize(true);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs_main);
+        tabLayout.setupWithViewPager(viewPager);
 
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(mLayoutManager);
-
-        UserService service = RetrofitTemplate.retrofit.create(UserService.class);
-        service.getUsers(token, new HashMap<String, String>())
-                .enqueue(new Callback<List<User>>() {
-                    @Override
-                    public void onResponse(Call<List<User>> call, Response<List<User>> response) {
-                        //Log.d("LOG_TAG", response.body().get(0).getName());
-                        loadUsers(response.body());
-                    }
-
-                    @Override
-                    public void onFailure(Call<List<User>> call, Throwable t) {
-                        Snackbar snackbar = Snackbar
-                                .make(linearLayout, "Data retrieval failed!", Snackbar.LENGTH_LONG);
-
-                        snackbar.show();
-                        String message = t.toString();
-                        Log.d("LOG_TAG", message);
-                    }
-                });
     }
 
-    private void loadUsers(List<User> users){
-        RecyclerAdapter adapter = new RecyclerAdapter(getApplicationContext(), users);
-        adapter.setClickListener(this);
-        recyclerView.setAdapter(adapter);
-    }
+    private void initializeViewPager(ViewPager viewPager, Toolbar toolbar) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
 
+        final String PEOPLE_TITLE = "PEOPLE";
+        String PROFILE_TITLE = "PROFILE";
+        String WALLET_TITLE = "WALLET";
 
-    @Override
-    public void itemClicked(View view, int position) {
-        Snackbar snackbar = Snackbar
-                .make(linearLayout, "Click working", Snackbar.LENGTH_LONG);
-
-        snackbar.show();
+        adapter.addFragment(new PeopleFragment(), PEOPLE_TITLE);
+        adapter.addFragment(new ProfileFragment(), PROFILE_TITLE);
+        adapter.addFragment(new WalletFragment(), WALLET_TITLE);
+        viewPager.setAdapter(adapter);
     }
 }
