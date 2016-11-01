34e929aaf39ce521d8bb0d84301dc489827abf97
Damilola Odujoko <odujoko.dml30@gmail.com>
2016-11-01 14:39:16 +0100

    Added Languages model

--- app/src/main/java/eu/shareonbazaar/dev/bazaar/model/JsonObject.java
+++ app/src/main/java/eu/shareonbazaar/dev/bazaar/model/JsonObject.java
@@ -3,31 +3,33 @@ package eu.shareonbazaar.dev.bazaar.model;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by Larry on 9/19/2016.
  */
 public class JsonObject {
-    @SerializedName("name")
+    @SerializedName("_id")
     @Expose
-    public String name;
+    public String id;
     @SerializedName("label")
     @Expose
-    public String label;
+    public Languages label;
 
-    public String getName() {
-        return name;
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLabel() {
+    public Languages getLabel() {
         return label;
     }
 
-    public void setLabel(String id) {
+    public void setLabel(Languages label) {
         this.label = label;
     }
 
---
+++ app/src/main/java/eu/shareonbazaar/dev/bazaar/model/Languages.java
@@ -0,0 +1,35 @@
+package eu.shareonbazaar.dev.bazaar.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Languages {
+    @SerializedName("ar")
+    @Expose
+    public String ar;
+    @SerializedName("de")
+    @Expose
+    public String de;
+    @SerializedName("en")
+    @Expose
+    public String en;
+
+    public String getAr() {
+        return ar;
+    }
+    public void setAr(String ar) {
+        this.ar = ar;
+    }
+    public String getDe() {
+        return de;
+    }
+    public void setDe(String de) {
+        this.de = de;
+    }
+    public String getEn() {
+        return en;
+    }
+    public void setEn(String en) {
+        this.en = en;
+    }
+}
