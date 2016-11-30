5b89fff3ff862d1c67d16a7aeacc1d1a83608839
OdujokoD <odujoko.dml30@gmail.com>
2016-11-30 23:35:07 +0100

    Add menu search icon resource

--- app/src/main/java/eu/shareonbazaar/dev/bazaar/activity/UsersActivity.java
+++ app/src/main/java/eu/shareonbazaar/dev/bazaar/activity/UsersActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import eu.shareonbazaar.dev.bazaar.R;
 import eu.shareonbazaar.dev.bazaar.utility.ViewPagerAdapter;
@@ -47,5 +48,25 @@ public class UsersActivity extends AppCompatActivity {
         adapter.addFragment(new ProfileFragment(), PROFILE_TITLE);
         adapter.addFragment(new WalletFragment(), WALLET_TITLE);
         viewPager.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_search:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
